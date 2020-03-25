ea8db2f016109be8aed2efb537562daed453c791
Thomas John Helin <thomashelin@yahoo.com>
2020-03-25 16:45:14 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,9 @@
+def turn_count(board)
+counter = 0
+board.each do |position|
+  if position == ("X") || position == ("O")
+counter += 1
+ end
+end
+counter
+end #returns the number of turns that have been played
